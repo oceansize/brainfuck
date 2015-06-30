6b2adc8933c8297d26de17d011a507546ed5654e
Roi Driscoll <roi.driscoll@gmail.com>
2015-06-30 22:53:02 +0100

    Refactors memory, adds pointer

--- lib/brainfuck.rb
+++ lib/brainfuck.rb
@@ -1,8 +1,14 @@
 class Brainfuck
 
-  attr_reader :namespace
+  attr_reader   :input
+  attr_accessor :memory, :pointer
 
   def initialize
-    @namespace = []
+    @memory  = Array.new(30000, 0)
+    @pointer = 0
+  end
+
+  def receive_instructions(user_string)
+    @input = user_string.split(//)
   end
 end
--- spec/brainfuck_spec.rb
+++ spec/brainfuck_spec.rb
@@ -2,8 +2,30 @@ require 'brainfuck'
 
 describe Brainfuck do
 
-  it 'uses an array as a namespace' do
-    brainfuck = Brainfuck.new
-    expect(brainfuck.namespace).to eq([])
+  let(:interpreter) { Brainfuck.new }
+
+  context 'by default' do
+
+    it 'accepts a string' do
+      interpreter.receive_instructions("+++")
+      expect(interpreter.input).to eq(["+", "+", "+"])
+    end
+
+    it 'has a memory array containing 30,000 cells' do
+      expect(interpreter.memory.size).to eq(30000)
+    end
+
+    it 'has a pointer which is initialised to zero' do
+      expect(interpreter.pointer).to eq(0)
+    end
+  end
+
+  context 'when it recognises valid input' do
+
+    it 'increments the value at the pointer when it sees a "+"' do
+      interpreter.receive_instructions("+")
+      interpreter.parse
+      expect(interpreter.memory.first).to eq(1)
+    end
   end
 end
