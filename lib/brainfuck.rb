50c93baf2ddc65326e62c57c69303aaf4557eeb0
Roi Driscoll <roi.driscoll@gmail.com>
2015-07-11 13:22:09 +0100

    To be revisited

--- lib/brainfuck.rb
+++ lib/brainfuck.rb
@@ -1,7 +1,11 @@
 class Brainfuck
 
-  attr_reader   :input
+  attr_reader   :interpreter_stream,
+                :increase_value
   attr_accessor :memory, :pointer
+
+  METHOD_LOOKUP = { "+" => IncreaseValue }
+
 
   def initialize
     @memory  = Array.new(30000, 0)
@@ -9,6 +13,36 @@ class Brainfuck
   end
 
   def receive_instructions(user_string)
-    @input = user_string.split(//)
+    @interpreter_stream = user_string.split(//)
   end
+
+  def increment_pointer
+    self.pointer += 1
+  end
+
+  def decrement_pointer
+    self.pointer -= 1
+  end
+
+  #def increase_value
+    #memory[pointer] += 1
+  #end
+
+  #@increase_value = Proc.new { memory[pointer] += 1 }
+
+  def decrease_value
+    memory[pointer] -= 1
+  end
+
+  def method_lookup
+    METHOD_LOOKUP["+"].call(memory)
+  end
+
+  def parse
+    interpreter_stream.each do |command|
+      sym = METHOD_LOOKUP[command]
+      send(sym)
+    end
+  end
+
 end
--- spec/brainfuck_spec.rb
+++ spec/brainfuck_spec.rb
@@ -1,3 +1,5 @@
+# http://fatiherikli.github.io/brainfuck-visualizer
+
 require 'brainfuck'
 
 describe Brainfuck do
@@ -8,7 +10,7 @@ describe Brainfuck do
 
     it 'accepts a string' do
       interpreter.receive_instructions("+++")
-      expect(interpreter.input).to eq(["+", "+", "+"])
+      expect(interpreter.interpreter_stream).to eq(["+", "+", "+"])
     end
 
     it 'has a memory array containing 30,000 cells' do
@@ -18,14 +20,51 @@ describe Brainfuck do
     it 'has a pointer which is initialised to zero' do
       expect(interpreter.pointer).to eq(0)
     end
+
+    it 'can send the pointer to an address in memory' do
+      increase_pointer_by_3
+      interpreter.memory[interpreter.pointer] = :test
+      expect(interpreter.memory[2]).to eq(:test)
+    end
+
+    it 'can increment the pointer' do
+      interpreter.increment_pointer
+      expect(interpreter.pointer).to eq(1)
+    end
+
+    it 'can decrement the pointer' do
+      increase_pointer_by_3
+      interpreter.decrement_pointer
+      expect(interpreter.pointer).to eq(1)
+    end
+
+    it 'can increase the value at a specific memory address' do
+      increase_pointer_by_3
+      interpreter.increase_value
+      expect(interpreter.memory[2]).to eq(1)
+    end
+
+    it 'can decrease the value at a specific memory address' do
+      increase_pointer_by_3
+      interpreter.increase_value
+      expect(interpreter.memory[2]).to eq(1)
+      interpreter.decrease_value
+      expect(interpreter.memory[2]).to eq(0)
+    end
   end
 
   context 'when it recognises valid input' do
 
-    it 'increments the value at the pointer when it sees a "+"' do
+    it 'increments the value at the current pointer when it sees a "+"' do
       interpreter.receive_instructions("+")
       interpreter.parse
       expect(interpreter.memory.first).to eq(1)
     end
   end
+
+  def increase_pointer_by_3
+    interpreter.pointer = 2
+  end
 end
+
+
