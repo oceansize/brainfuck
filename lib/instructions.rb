b2be6ba834732674c88bd157aca69c6900949675
Roi Driscoll <roi.driscoll@gmail.com>
2017-02-11 18:34:28 +0000

    Adds checks for incorrect instructions

--- lib/instructions.rb
+++ lib/instructions.rb
@@ -3,6 +3,20 @@ class Instructions
   attr_reader :sequence
 
   def initialize(instructions)
-    @sequence = instructions.split(//)
+    @sequence = sanitize(instructions)
+  end
+
+  private
+
+  def sanitize(instructions)
+    raise "Invalid syntax" unless instructions.is_a?(String)
+    validate(instructions)
+  end
+
+  def validate(instructions)
+    sequence = instructions.split(//)
+    sequence.each do |command|
+      raise "Invalid command" unless Brainfuck::METHOD_LOOKUP.has_key?(command)
+    end
   end
 end
--- spec/instructions_spec.rb
+++ spec/instructions_spec.rb
@@ -2,9 +2,17 @@ require 'instructions'
 
 describe Instructions do
 
-    it 'accepts a string' do
+    it "Converts input into a sequence of instructions" do
       stream = Instructions.new("+++")
       expect(stream.sequence).to eq(["+", "+", "+"])
+    end
+
+    it "will not accept anything else" do
+      expect { Instructions.new(666) }.to raise_error("Invalid syntax")
+    end
+
+    it "will reject incorrect syntax" do
+      expect { Instructions.new("+~+") }.to raise_error("Invalid command")
     end
 
 end
